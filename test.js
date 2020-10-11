abe54ed6b7f3b966cc9bbdcbf9b9a5aa7f2e52ff
Praharsh Singh <praharsh535@gmail.com>
2020-10-11 19:56:54 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,13 @@
+$(".animationText").not(".slick-initialized").slick({
+  autoplay: true,
+  autoplaySpeed: 1500,
+  vertical: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  adaptiveHeight: true,
+  // fade: true,
+  // centerMode: true,
+  // centerMargin: "30px",
+  prevArrow: ".site-slider .slider-btn .prev",
+  nextArrow: ".site-slider .slider-btn .next",
+});
