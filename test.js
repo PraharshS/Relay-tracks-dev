b39375fce908288d28beeb836c62c6bdcfc87acd
Praharsh Singh <praharsh535@gmail.com>
2020-10-11 22:48:22 +0530

    2nd page added

--- test.js
+++ test.js
@@ -5,9 +5,28 @@ $(".animationText").not(".slick-initialized").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   adaptiveHeight: true,
+
   // fade: true,
   // centerMode: true,
   // centerMargin: "30px",
   prevArrow: ".site-slider .slider-btn .prev",
   nextArrow: ".site-slider .slider-btn .next",
 });
+
+$(".shortSlider").not(".slick-initialized").slick({
+  autoplay: true,
+  autoplaySpeed: 4000,
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  // centerMode: true,
+});
+// $(".sliderImagesBig").not(".slick-initialized").slick({
+//   autoplay: true,
+//   autoplaySpeed: 3000,
+//   dots: false,
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   // centerMode: true,
+// });
